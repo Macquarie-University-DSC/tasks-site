b824e8d4452de382d195ee55acffeea33e22a4b7
Euan Mendoza <bebopbamf@effectfree.dev>
2021-06-26 01:44:04 +1000

    added toggling checkbox

--- src/Main.elm
+++ src/Main.elm
@@ -6,10 +6,18 @@ import Html.Attributes exposing (class, type_, checked)
 import Html.Events exposing (onClick)
 import Task
 import Time
-
+import List exposing ((::))
 
 -- MODEL --
 
+
+testTasks : List TaskType
+testTasks =
+  [ TaskType 1 "Test Task 1" "Test Task 1 Description" Nothing False
+  , TaskType 2 "Test Task 2" "Test Task 2 Description" Nothing True
+  , TaskType 3 "Test Task 3" "Test Task 3 Description" (Just (Time.millisToPosix 1625097600000)) False
+  , TaskType 4 "Test Task 4" "Test Task 4 Description" (Just (Time.millisToPosix 1625097600000)) True
+  ]
 
 type alias Model =
   { tasks : List TaskType
@@ -17,7 +25,8 @@ type alias Model =
   }
 
 type alias TaskType =
-  { name        : String
+  { id          : Int
+  , name        : String
   , description : String
   , due_date    : Maybe Time.Posix
   , is_complete : Bool
@@ -36,6 +45,7 @@ init _ =
 
 
 type Msg = AdjustTimeZone Time.Zone
+         | ToggleComplete Int Bool
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -44,6 +54,18 @@ update msg model =
       ( { model | zone = newZone }
       , Cmd.none
       )
+
+    ToggleComplete id is_complete ->
+      let
+        updateTask task =
+          if task.id == id then
+            { task | is_complete = is_complete }
+          else
+            task
+        in 
+          ( { model | tasks = List.map updateTask model.tasks }
+          , Cmd.none
+          )
 
 
 
@@ -59,30 +81,21 @@ subscriptions _ =
 -- VIEW --
 
 
-testTasks : List TaskType
-testTasks =
-  [ TaskType "Test Task 1" "Test Task 1 Description" Nothing False
-  , TaskType "Test Task 2" "Test Task 2 Description" Nothing True
-  , TaskType "Test Task 3" "Test Task 3 Description" (Just (Time.millisToPosix 1625097600000)) False
-  , TaskType "Test Task 4" "Test Task 4 Description" (Just (Time.millisToPosix 1625097600000)) True
-  ]
-
 view : Model -> Html Msg
 view model =
   div [ class "flex-main" ]
-    (List.append 
-      ([ h1 [] [ text "TRACK YOUR TASKS!" ]
-      ]) (viewTasks model))
+    (viewTasks model)
 
 viewTasks : Model -> List (Html Msg)
 viewTasks model =
-  List.map (viewTask model) testTasks
+  (h1 [] [ text "TRACK YOUR TASKS!" ]) :: (List.map (viewTask model) model.tasks)
 
 viewTask : Model -> TaskType -> Html Msg
 viewTask _ task =
   div [ class "flex-row" ]
-    [ input [ type_ "checkbox", checked task.is_complete ] []
+    [ input [ type_ "checkbox", checked task.is_complete, onClick (ToggleComplete task.id (not task.is_complete)) ] []
     , h4 [] [ text task.name ]
+    , button [] [ text "v" ]
     ]
 
 
